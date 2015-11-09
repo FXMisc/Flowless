a8e8e8a52380642eff0313414e251439d876797c
JordanMartinez <jordanalex.martinez@gmail.com>
2015-11-09 14:48:24 -0800

    Removed unneeded imports

--- src/main/java/org/fxmisc/flowless/Virtualized.java
+++ src/main/java/org/fxmisc/flowless/Virtualized.java
@@ -1,10 +1,7 @@
 package org.fxmisc.flowless;
 
-import javafx.geometry.Orientation;
 import org.reactfx.value.Val;
 import org.reactfx.value.Var;
-
-import java.util.Optional;
 
 public interface Virtualized {
     Val<Double> totalWidthEstimateProperty();
