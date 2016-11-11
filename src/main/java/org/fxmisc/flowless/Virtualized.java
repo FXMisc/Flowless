4f7d2c4b69de86f224dd1940bbe33f19c855346d
Jordan Martinez <jordanalex.martinez@gmail.com>
2016-11-11 08:17:56 -0800

    Include convenience methods for scrolling

--- src/main/java/org/fxmisc/flowless/Virtualized.java
+++ src/main/java/org/fxmisc/flowless/Virtualized.java
@@ -1,5 +1,6 @@
 package org.fxmisc.flowless;
 
+import javafx.geometry.Point2D;
 import org.reactfx.value.Val;
 import org.reactfx.value.Var;
 
@@ -8,6 +9,25 @@ public interface Virtualized {
     Val<Double> totalHeightEstimateProperty();
     Var<Double> estimatedScrollXProperty();
     Var<Double> estimatedScrollYProperty();
+
+    /**
+     * Convenience method: scroll horizontally by {@code deltas.getX()} and vertically by {@code deltas.getY()}
+     * @param deltas negative values scroll left/up, positive scroll right/down
+     */
+    default void scrollBy(Point2D deltas) {
+        scrollXBy(deltas.getX());
+        scrollYBy(deltas.getY());
+    }
+
+    /**
+     * Convenience method: scroll horizontally by {@code deltaX} and vertically by {@code deltaY}
+     * @param deltaX negative values scroll left, positive scroll right
+     * @param deltaY negative values scroll up, positive scroll down
+     */
+    default void scrollBy(double deltaX, double deltaY) {
+        scrollXBy(deltaX);
+        scrollYBy(deltaY);
+    }
 
     /**
      * Scroll the content horizontally by the given amount.
@@ -20,6 +40,22 @@ public interface Virtualized {
      * @param deltaY positive value scrolls down, negative value scrolls up
      */
     void scrollYBy(double deltaY);
+
+    /**
+     * Convenicen method: scroll the content to the pixel
+     */
+    default void scrollToPixel(Point2D pixel) {
+        scrollXToPixel(pixel.getX());
+        scrollYToPixel(pixel.getY());
+    }
+
+    /**
+     * Convenicen method: scroll the content to the pixel
+     */
+    default void scrollToPixel(double xPixel, double yPixel) {
+        scrollXToPixel(xPixel);
+        scrollYToPixel(yPixel);
+    }
 
     /**
      * Scroll the content horizontally to the pixel
